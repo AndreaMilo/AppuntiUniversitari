68c1a84e6361911dce3aa7da5b3ee03ec24793a6
AndreaMilo <andreagiomilo@gmail.com>
2026-04-17 10:59:25 +0200

    vault backup: 2026-04-17 10:59:25

--- Secondo anno/secondo trimestre/Ingegneria del software/VsCodeEx/uvTest/Hello.py
+++ Secondo anno/secondo trimestre/Ingegneria del software/VsCodeEx/uvTest/Hello.py
@@ -1,4 +1,7 @@
 import random
 import rich
+import time
+
 contacts = ["John Doe", "Jane Smith", "Alice Johnson"]
 rich.print(f"Hello [bold blue]{random.choice(contacts)}![/bold blue]")
+print("Hello pablo is "+ (time.ctime()))
